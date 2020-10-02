6bcb6b5a18c3e69eb366bbda24eee09713827bfe
Zaki Maulana <zakimaulana08@gmail.com>
2020-10-02 15:26:44 +0700

    rapihin aja

--- koneksi.php
+++ koneksi.php
@@ -4,5 +4,22 @@ $username = "root";
 $password = ""; 
 $database = "db_arkademy";
 
-$pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
+// Koneksi ke MySQL dengan PDO
+try {
+    $pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
+
+$sql = "CREATE TABLE IF NOT EXISTS produk (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    nama_produk VARCHAR(15) NOT NULL,
+    keterangan TEXT NOT NULL,
+    harga VARCHAR(15),
+    jumlah VARCHAR(3)
+)";
+
+$pdo->exec($sql);
 ?>
